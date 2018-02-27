5dcaa836b5acfdde2d8ee7a77f3c18ac8090d473
Victor <victorasantekyei@yahoo.co.uk>
2018-02-27 14:58:53 +0100

    doch nicht

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
@@ -31,11 +31,11 @@ var options = { allowInsecureHTTP: true };
 
 var dashboard = new ParseDashboard({
   //By all means disable this one production!!!!!
-  "allowInsecureHTTP": false,
+  "allowInsecureHTTP": true,
   //to run multiple apps on server, add new app object here
   "apps": [
     {
-      "serverURL": process.env.SERVER_URL || 'https://localhost:1337/parse',
+      "serverURL": process.env.SERVER_URL || 'http://localhost:1337/parse',
       "appId": "myAppId",
       "masterKey": "myMasterKey",
       "appName": "MyApp"
